06a37524c175fcf5d2737001031f126013b9d6c3
Damian Dobrew <176309739+ejhuus@users.noreply.github.com>
2025-03-18 12:30:39 +0100

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,76 @@
+from typing import Any, Union
+
+# Pomocnicza funkcja, dodająca spację na początku każdej linijki w napisie.
+def indent(s: str) -> str:
+    return '\n'.join(' '+line for line in s.splitlines())
+
+
+class Leaf:
+    # Pola poniżej należą do instancji, nie do klasy! (nie są statyczne)
+    # Gdyby nie było typów, zmienna zadeklarowana tutaj byłaby statyczna.
+    value: Any
+    size: int
+    
+    def __init__(self, value: Any):
+        self.value = value
+        self.size = -1
+
+    # Prosta pomocnicza funkcja do wypisywania (pod)drzewa (można napisać własną, bardziej czytelną); również w RegBinNode.
+    def __str__(self):
+        return f'{self.value} (size: {self.size})\n'
+
+    def calc_node(self):
+        self.size = 1
+        return self.size
+
+
+class RegBinNode:
+    # Pola poniżej należą do instancji, nie do klasy! (nie są statyczne)
+    value: Any
+    left: Union['RegBinNode',Leaf]
+    right: Union['RegBinNode',Leaf]
+    size: int
+
+    def __init__(self,
+                 value: Any,
+                 left: Union['RegBinNode',Leaf], 
+                 right: Union['RegBinNode',Leaf]):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.size = -1
+    
+    def __str__(self):
+        return f'{self.value} (size: {self.size})\n{indent(str(self.left))}\n{indent(str(self.right))}'
+
+    def calc_node(self):
+        result = 1
+        if self.left:
+            result += self.left.calc_node()
+        if self.right:
+            result += self.right.calc_node()
+        self.size=result
+        return self.size
+
+
+class RegBinTree:
+    # Pole "root" należy do instancji, nie do klasy! (nie jest statyczne)
+    root: RegBinNode|Leaf|None
+
+    def __init__(self, node: RegBinNode|Leaf|None = None):
+        self.root = node
+    
+    def calculate_sizes(self):
+        # TODO: uzupełnij tę metodę (możesz deklarować dodatkowe metody tutaj, w PosBinNode lub globalnie).
+        if self.root is None:
+            return None
+        self.root.calc_node()
+        pass
+
+    def find_centroid(self) -> RegBinNode|Leaf|None:
+        # TODO: uzupełnij tę metodę (możesz deklarować dodatkowe metody tutaj, w PosBinNode lub globalnie).
+        # Możesz założyć, że pola 'size' w wierzchołkach zawierają prawidłowe wartości.
+        return None
+
+    def __str__(self):
+        return '(Empty tree)' if self.root is None else str(self.root)
